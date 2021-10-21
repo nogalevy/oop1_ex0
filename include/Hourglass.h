6258d715e14e96be28cb3a0c5498711df141fd39
Tali Kalev <talkalev@gmail.com>
2021-10-21 17:46:54 +0300

    added steps to triangle and hourglass header

---
+++ include/Hourglass.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Vertex.h"
+#include "Triangle.h"
+
+class Hourglass {
+public:
+	Hourglass(const Triangle& upper, const Triangle& lower); //for sure received valid traingles
+
+
+private:
+};
