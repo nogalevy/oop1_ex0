49469c43dcfb0d219886ab872e4ac392cc43232d
nogalevy <nogalevy2@gmail.com>
2021-10-27 13:26:08 +0300

    add comments and fixed height and length calc for rectangle

--- include/Rectangle.h
+++ include/Rectangle.h
@@ -1,53 +1,39 @@
-#pragma once
-#include "Utilities.h"
+	#pragma once
+	#include "Utilities.h"
 
-#include "Vertex.h"
-#include "Board.h"
+	#include "Vertex.h"
+	#include "Board.h"
 
-const int DEFAULT_LEN = 10;
-
-class Rectangle
+	class Rectangle
 {
 public:
 	//===--- constructors ---===//
-	
 	Rectangle(const Vertex& bottomLeft, const Vertex& topRight);
-	
 	Rectangle(const Vertex vertices[2]);
-	
 	Rectangle(double x0, double y0, double x1, double y1);
-	
 	Rectangle(const Vertex& start, double width, double height);
 
 	//===--- functions ---===//
-  //return BL vertex
-  Vertex getBottomLeft()const;
-  //return TR vertex
-  Vertex getTopRight()const;
-  //return width (Xr - Xl)
-  double getWidth()const;
-  //return height (Yt - Yb)
-  double getHeight()const;
+	Vertex getBottomLeft()const;
+	Vertex getTopRight()const;
+	double getWidth()const;	 // the side : ___
+	double getHeight()const; // the side : |
 	
-
-  void draw(Board& board)const;
-  Rectangle getBoundingRectangle() const;
-  double getArea()const;
-  double getPerimeter()const;	
-  Vertex getCenter() const;
-  bool scale(double factor);
-
+	void draw(Board& board)const;
+	Rectangle getBoundingRectangle() const;
+	double getArea()const;
+	double getPerimeter()const;	
+	Vertex getCenter() const;
+	bool scale(double factor);
 
 private:
-	//
 	//members
+	Vertex m_bottomLeft; 
+	Vertex m_topRight; 
+	double m_width;
+	double m_height;
 
-  Vertex m_bottomLeft; //bottom left
-  Vertex m_topRight; //top right
-
-  double m_width;
-  double m_height;
-  //--- sub functions ---
+	//sub functions
 	void saveDefault();
 	void saveVertices(const Vertex& bottomLeft, const Vertex& topRight);
 };
--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -1,11 +1,9 @@
 
 #include "Rectangle.h"
 
-#include <iostream>
+// -----================ constructors ================-----
 
-
-//------------- constructor ------------------
-
+//----
 Rectangle::Rectangle (const Vertex& bottomLeft, const Vertex& topRight)
     : m_bottomLeft(bottomLeft), m_topRight(topRight) 
 {
@@ -18,56 +16,56 @@ Rectangle::Rectangle (const Vertex& bottomLeft, const Vertex& topRight)
   saveDefault();
 }
 
+//----
 Rectangle::Rectangle (const Vertex vertices[2])
-  : Rectangle(vertices[0], vertices[1])
+    : Rectangle(vertices[0], vertices[1])
 { }
 
+//----
 Rectangle::Rectangle (double x0, double y0, double x1, double y1)
     : Rectangle(Vertex(x0, y0), Vertex(x1, y1))
 { }
 
-
-// !!! ask about the Vertex-auto
+//----
 Rectangle::Rectangle (const Vertex& start, double width, double height)
 {
-    //auto t_r = Vertex(start.m_col + width, start.m_row + height);
     if (width < 0 || height < 0)
         saveDefault();
 
-   saveVertices(start, Vertex(start.m_col + width, start.m_row + height));
+    saveVertices(start, Vertex(start.m_col + width, start.m_row + height));
 }
 
-//------------------- main functions -------------------
+// -----================ main functions (public) ================-----
 
- //return BL vertex
+//----
 Vertex Rectangle::getBottomLeft()const
 {
     return m_bottomLeft;
 }
 
-//return TR vertex
+//----
 Vertex Rectangle::getTopRight()const
 {
     return m_topRight;
 }
 
-//use distance in utilitties
-//return width (Xr - Xl)
+//----
 double Rectangle::getWidth()const
 {
-    return m_topRight.m_col - m_bottomLeft.m_col;
+    return m_width;
 }
-//return height (Yt - Yb)
+
+//----
 double Rectangle::getHeight()const
 {
-    return  m_topRight.m_row - m_bottomLeft.m_row;
+    return m_height;
 }
 
-
-//draw the Rectangle
+//----
 void Rectangle::draw(Board& board)const
 {
-    //BL -> TL    //TL -> TR    //TR -> BR    //BR -> BL
+    //draw:
+    //b_l -> t_l   t_l -> t_r    t_r -> b_r   b_r -> b_l
     Vertex t_l = Vertex(m_bottomLeft.m_col, m_topRight.m_row),
             b_r = Vertex(m_topRight.m_col, m_bottomLeft.m_row);;
 
@@ -77,22 +75,25 @@ void Rectangle::draw(Board& board)const
     board.drawLine(b_r, m_bottomLeft);
 }
 
-
+//----
 Rectangle Rectangle::getBoundingRectangle() const
 {
     return *this;
 }
 
+//----
 double Rectangle::getArea()const
 {
-    return m_height* m_width;
+    return (m_height * m_width);
 }
 
+//----
 double Rectangle::getPerimeter()const
 {
-    return 2*m_height + 2*m_width;
+    return (2*m_height + 2*m_width);
 }
 
+//----
 Vertex Rectangle::getCenter()const
 {
     double x = m_bottomLeft.m_col + (m_width / 2);
@@ -101,6 +102,7 @@ Vertex Rectangle::getCenter()const
     return Vertex(x, y);
 }
 
+//----
 bool Rectangle::scale(double factor)
 {
     Vertex center = getCenter();
@@ -111,8 +113,6 @@ bool Rectangle::scale(double factor)
     new_b_l = Vertex(center.m_col - (center.m_col - m_bottomLeft.m_col) * factor, center.m_row - (center.m_row - m_bottomLeft.m_row) * factor);
     new_t_r = Vertex(center.m_col + (m_topRight.m_col - center.m_col) * factor, center.m_row + (m_topRight.m_row - center.m_row) * factor);
  
-    std::cout << new_b_l.m_col;
-
     if (!new_b_l.isValid() || !new_t_r.isValid()) return false;
 
     m_bottomLeft = new_b_l;
@@ -121,19 +121,23 @@ bool Rectangle::scale(double factor)
     return true;
 }
 
-//------------------------ sub function -------------------------------
+
+// -----================ sub functions (private) ================-----
+
+//----
 void Rectangle::saveDefault()
 {
     saveVertices(Vertex(20, 10), Vertex(30, 20));
-    m_height = m_width = DEFAULT_LEN;
 }
 
+//----
 void Rectangle::saveVertices(const Vertex& bottomLeft, const Vertex& topRight)
 {
-  m_bottomLeft = bottomLeft;
-  m_topRight = topRight;
+    auto b_r = Vertex(m_topRight.m_col, m_bottomLeft.m_row);
+    m_bottomLeft = bottomLeft;
+    m_topRight = topRight;
 
-  m_height = m_topRight.m_row - m_bottomLeft.m_row;  // y
-  m_width = m_topRight.m_col - m_bottomLeft.m_col;  // x
+    m_height = distance(b_r, m_topRight); // y
+    m_width = distance(b_r, m_topRight);  // x
 }
 
